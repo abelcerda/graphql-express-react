c1135e48ba6aa6782816d037d50d3407651bda1b
Abel Antonio Cerda <abelantoniocerda@gmail.com>
2022-03-31 21:25:06 -0300

    Update app component to open form to create and edit posts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import './App.css';
+import { useState } from 'react';
+import { Button, Container } from 'reactstrap';
+
+import PostEditor from './PostEditor';
 import PostViewer from './PostViewer';
 
 function App() {
+  const [editing, setEditing] = useState(null);
+
   return (
-    <main>
-      <PostViewer />
-    </main>
+    <Container fluid>
+      <Button
+        className='my-2'
+        color='primary'
+        onClick={() => setEditing({})}
+      >
+        New Post
+      </Button>
+      <PostViewer
+        canEdit={() => true}
+        onEdit={(post) => setEditing(post)}
+      />
+      {editing && (
+        <PostEditor
+          post={editing}
+          onClose={() => setEditing(null)}
+        />
+      )}
+    </Container>
   );
 }
 
